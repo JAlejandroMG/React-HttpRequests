04a692a062997bcd73cd227893e83065ae0e5b3c
JAlejandroMG <jalejandromg@tutanota.com>
2021-07-31 21:52:41 -0500

    Add AddMovie component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
+import MoviesList from './components/MoviesList';
 
 
 
@@ -24,7 +25,8 @@ function App() {
     setError(null);
 
     try{
-      const response = await fetch('https://swapi.dev/api/films/');
+      // const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-maxs-default-rtdb.firebaseio.com/movies.json');
       
       if (!response.ok) {
         throw new Error("Something went wrong!")
@@ -52,6 +54,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  };
+
   let content = <p>There are no movies yet!</p>
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />
@@ -66,6 +72,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
